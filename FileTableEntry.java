cea68275ef9300ddbb478b2b511de55e1d45e564
gennyBrown <45495508+gennyBrown@users.noreply.github.com>
2020-12-15 18:28:11 -0800

    finished SysLib, Kernel, fileTable.format

--- FileTableEntry.java
+++ FileTableEntry.java
@@ -24,12 +24,12 @@ public class FileTableEntry {
 
 
     public FileTableEntry ( Inode i, short inumber, String m ) {
-        seekPtr = 0;             // the seek pointer is set to the file top
+        seekPtr = 0;                        // the seek pointer is set to the file top
         inode = i;
         iNumber = inumber;
-        count = 1;               // at least on thread is using this entry
-        mode = m;                // once access mode is set, it never changes
-        if ( mode.compareTo( "a" ) == 0 ) // if mode is append,
-            seekPtr = inode.length;        // seekPtr points to the end of file
+        count = 1;                          // at least on thread is using this entry
+        mode = m;                           // once access mode is set, it never changes
+        if ( mode.compareTo( "a" ) == 0 )   // if mode is append,
+            seekPtr = inode.fileLength;         // seekPtr points to the end of file
     }
 }
